0ff712e26b9522fd544aad23b9667347a4cf18e8
nfahle <nf@bytediscover.de>
2020-08-18 12:30:22 +0200

    Updates

---
+++ precache-manifest.ab2d8ab042f3c135115f137fdbe33106.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7891f77c4ca3fada1c74c03596ed66de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45b30f8b937ebfb18945",
+    "url": "./static/css/2.c8cfea30.chunk.css"
+  },
+  {
+    "revision": "fa03d63958be0508510b",
+    "url": "./static/css/main.49a94b28.chunk.css"
+  },
+  {
+    "revision": "45b30f8b937ebfb18945",
+    "url": "./static/js/2.cfdfc0fe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.cfdfc0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa03d63958be0508510b",
+    "url": "./static/js/main.5eafb6e1.chunk.js"
+  },
+  {
+    "revision": "117b9c1939531a3b10e2",
+    "url": "./static/js/runtime-main.e9c5ec6a.js"
+  },
+  {
+    "revision": "ae28ce512154057612769a2330d271ed",
+    "url": "./static/media/background.ae28ce51.png"
+  }
+]);
