ce9ef8e8626a2e9d24e04994d1b344f0191cea4d
Cyklan <37187056+Cyklan@users.noreply.github.com>
2022-07-14 21:19:35 +0200

    Conditionally render widget or builder

---
+++ src/lib/Header.svelte
@@ -0,0 +1,3 @@
+<header class="w-screen h-16 bg-gray-200 grid place-items-center">
+  <h1 class="font-medium uppercase text-2xl">Songify Widget Builder</h1>
+</header>
---
+++ src/lib/Main.svelte
@@ -0,0 +1,9 @@
+<script>
+
+import Header from "./Header.svelte";
+
+</script>
+<div class="h-screen max-h-screen">
+  <Header />
+  <main />
+</div>
