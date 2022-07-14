a5b7385157b9c2f14de8ed2cd9d8f71fdff65672
Cyklan <37187056+Cyklan@users.noreply.github.com>
2022-07-14 21:42:37 +0200

    fuck svelte, all my homies hate svelte

--- src/lib/Main.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-
-import Header from "./Header.svelte";
-
-</script>
-<div class="h-screen max-h-screen">
-  <Header />
-  <main />
-</div>
--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  purge: ["./index.html", "./src/**/*.{svelte,js,ts}"],
-  plugins: [],
-}
